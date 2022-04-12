3f3ce1af93870a2f581027e0fd80e4c7b3b7ee96
youngwooo11 <jakejyw2002@naver.com>
2022-04-12 13:03:03 +0900

    first

---
+++ 돌쇠 4주차 중급.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char ABC;
+	cout << "대문자를 입력하세요.: ";
+	cin >> ABC;
+	cout << "대문자 " << ABC << "의 소문자는 " << char(ABC + 32) << "입니다.";
+
+
+
+
+
+
+
+
+}
---
+++ 돌쇠 4주차 초급.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int A, B;
+	cout << "첫번째 숫자를 입력하세요: ";
+	cin >> first;
+	cout << "두번째 숫자를 입력하세요: ";
+	cin >> second;
+	cout << A << " + " << B << " = " << A + B << endl;
+	cout << A << " - " << B << " = " << A - B << endl;
+	cout << A << " * " << B << " = " << A * B << endl;
+	cout << A << " / " << B << " = " << A / B << endl;
+	cout << A << " % " << B << " = " << A % B << endl;
+
+	return 0;
+}
